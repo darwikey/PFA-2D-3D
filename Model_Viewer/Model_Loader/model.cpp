9b1a700bef7d11022bb3287fadb12c0e1c7712f7
darwikey <xavier.maupeu@wanadoo.fr>
2014-12-03 16:40:35 +0100

    nettoyage du depot

--- Model_Viewer/Model_Loader/model.cpp
+++
@@ -1,8 +0,0 @@
-#include "model.h"
-
-Model::Model(string filePath)
-{
-        //loadOBJ(filePath.c_str(), vertices, uvs, normals);
-        loadPLY(filePath.c_str(), vertices);
-        m_fileName = filePath;
-}
