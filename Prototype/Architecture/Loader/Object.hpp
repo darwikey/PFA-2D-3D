b1670810e370e99d223a3722a014d0cf43f6fcc1
darwikey <xavier.maupeu@wanadoo.fr>
2014-12-27 11:43:10 +0100

    maj Objet.hpp

--- Prototype/Architecture/Loader/Object.hpp
+++ Prototype/Architecture/Loader/Object.hpp
@@ -3,6 +3,7 @@
 
 
 #include "global.hpp"
+#include "BoundingBox.hpp"
 
 //! \class Object
 //! \brief definition of objects that will be placed in the scene
@@ -15,7 +16,13 @@ class Object{
 	moveObject(float horizontalValue, float verticalValue, float depthValue);
 
 	private:
+	BoundingBox mBoundingBox;
 	
+	std::vector<QVector3D> mVertices;
+    std::vector<QVector2D> mUvs;
+    std::vector<QVector3D> mNormals;
+    std::vector<uint> mIndices;
+    std::vector<QVector3D> mColor;
 }
 
 
