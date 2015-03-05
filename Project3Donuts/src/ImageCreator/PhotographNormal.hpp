8d86b4d35a88a64e8ad4032059e7743df895f351
darwikey <xavier.maupeu@wanadoo.fr>
2015-03-05 18:32:21 +0100

    Ajout class Photograh

---
+++ Project3Donuts/src/ImageCreator/PhotographNormal.hpp
@@ -0,0 +1,32 @@
+#ifndef PHOTOGRAPH_NORMAL_HPP
+#define PHOTOGRAPH_NORMAL_HPP
+
+#include "global.hpp"
+#include "Photograph.hpp"
+
+
+// ! \class PhotographNormal
+// ! \brief Create a simple render of the scene
+class PhotographNormal : public Photograph{
+	Q_OBJECT
+
+protected:
+
+	virtual void createWindow() override;
+
+	virtual std::shared_ptr<QImage> render() override;
+
+
+private:
+	QCheckBox* mGreyBox = nullptr;
+
+	bool mIsGrey = false;
+
+
+private slots:
+	void setGrey(int fIsGrey);
+
+
+};
+
+#endif
