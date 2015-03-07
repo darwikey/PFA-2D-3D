a13a742155805f3cec75ae6430bdd5d723efcb44
darwikey <xavier.maupeu@wanadoo.fr>
2015-03-07 14:24:31 +0100

    Ajout commentaires

--- Project3Donuts/src/ImageCreator/CreationFile.hpp
+++ Project3Donuts/src/ImageCreator/CreationFile.hpp
@@ -3,20 +3,26 @@
 
 #include "global.hpp"
 
-
+//! \class Creation file
+//! \brief can contain an image or an animated image
 class CreationFile{
 public:
 	enum class Type{
 		IMAGE, ANIMATED_GIF
 	};
 
+	//! \brief Constructor
+	//! \param the type of image that the class will contain
 	CreationFile(Type fType);
 
-
+	//! \brief add an image
 	void pushImage(std::shared_ptr<QImage> fImage);
 
+	//! \brief save the content of the class into a file
+	//! \param the path of the file
 	void save(const QString& fFileName);
 
+	//! \brief return the first image add 
 	std::shared_ptr<QImage> getFirstImage();
 
 
