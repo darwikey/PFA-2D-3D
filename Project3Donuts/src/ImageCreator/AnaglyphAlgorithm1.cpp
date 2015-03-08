401d1d2afaf52f53cdebad1b7aeed87d2e7188de
AlexandrePhilippi <alex.93metz@hotmail.fr>
2015-03-08 21:55:56 +0100

    Anaglyph photoshop operationnal

--- Project3Donuts/src/ImageCreator/AnaglyphAlgorithm1.cpp
+++ Project3Donuts/src/ImageCreator/AnaglyphAlgorithm1.cpp
@@ -10,7 +10,7 @@ void AnaglyphAlgorithm1::createWindow(bool fHasPreview){
 
 std::unique_ptr<CreationFile> AnaglyphAlgorithm1::render(){
 	std::unique_ptr<QImage> _left = this->getColorMap();
-	std::unique_ptr<QImage> _right = this->getColorMap(1.0, 0.0, 1.0);
+	std::unique_ptr<QImage> _right = this->getColorMap(0.0, 7.0, 1.0);
 
 	std::unique_ptr<QImage> _image(new QImage(_left->size().width(),
 																						_left->size().height(),
@@ -20,7 +20,7 @@ std::unique_ptr<CreationFile> AnaglyphAlgorithm1::render(){
 		{
 			for(int j=0; j<_image->size().width(); j++)
 				{
-					QRgb value = qRgb(qRed(_image->pixel(j,i)),
+					QRgb value = qRgb(qRed(_left->pixel(j,i)),
 														qGreen(_right->pixel(j,i)),
 														qBlue(_right->pixel(j,i)));
 					
