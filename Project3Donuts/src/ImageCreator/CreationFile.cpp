31966e32ba4a8f22845680489f2bd321ff20e2a2
AlexandrePhilippi <alex.93metz@hotmail.fr>
2015-03-27 21:22:17 +0100

    les gifs marche, limite a 500 images max sinon lag

--- Project3Donuts/src/ImageCreator/CreationFile.cpp
+++ Project3Donuts/src/ImageCreator/CreationFile.cpp
@@ -12,71 +12,71 @@ CreationFile::CreationFile(const CreationFile& fInstance) {
 
 
 CreationFile::~CreationFile(){
-	for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
-		delete *_im;
-	}
+  for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
+    delete *_im;
+  }
 }
 
 
 void CreationFile::pushImage(std::unique_ptr<QImage> fImage){
-	mImages.push_back(fImage.release());
+  mImages.push_back(fImage.release());
 }
 
 
 void CreationFile::save(const QString& fFileName){
-	switch (mType){
-	case Type::IMAGE:
-		{
-			if (!mImages.empty()){
-				mImages.front()->save(fFileName);
-			}
-		}
-		break;
+  switch (mType){
+  case Type::IMAGE:
+    {
+      if (!mImages.empty()){
+        mImages.front()->save(fFileName);
+      }
+    }
+    break;
 
-	case Type::ANIMATED_GIF:
-		{
-			if (!mImages.empty()){
-				GifWriter _gif;
-				_gif.GifBegin(fFileName.toStdString().c_str(), mImages.front()->width(), mImages.front()->height(), (int)mDelayBetweenFrame);
+  case Type::ANIMATED_GIF:
+    {
+      if (!mImages.empty()){
+        GifWriter _gif;
+        _gif.GifBegin(fFileName.toStdString().c_str(), mImages.front()->width(), mImages.front()->height(), (int)mDelayBetweenFrame);
 
-				for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
-					_gif.GifWriteFrame(*_im);
-				}
+        for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
+          _gif.GifWriteFrame(*_im);
+        }
 
-				_gif.GifEnd();
-			}
-		}
-		break;
+        _gif.GifEnd();
+      }
+    }
+    break;
 
-	case Type::SEPARATED_IMAGE:
-		{
-			std::string _filename = fFileName.toStdString();
+  case Type::SEPARATED_IMAGE:
+    {
+      std::string _filename = fFileName.toStdString();
 
-			auto _dot = _filename.find_last_of('.');
-			if (_dot != std::string::npos){
-				_filename = _filename.substr(0, _dot);
-			}
+      auto _dot = _filename.find_last_of('.');
+      if (_dot != std::string::npos){
+        _filename = _filename.substr(0, _dot);
+      }
 
-			if (!mImages.empty()){
-				int _i = 1;
-				for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
-					(*_im)->save(QString::fromStdString(_filename + std::to_string(_i) + ".png"));
-					_i++;
-				}
-			}
-		}
+      if (!mImages.empty()){
+        int _i = 1;
+        for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
+          (*_im)->save(QString::fromStdString(_filename + std::to_string(_i) + ".png"));
+          _i++;
+        }
+      }
+    }
 
-	default:
-		break;
-	}
+  default:
+    break;
+  }
 }
 
 
 const QImage* CreationFile::getFirstImage() const{
-	if (mImages.empty()){
-		return nullptr;
-	}
-	else{
-		return mImages.front();
-	}
+  if (mImages.empty()){
+    return nullptr;
+  }
+  else{
+    return mImages.front();
+  }
 }
