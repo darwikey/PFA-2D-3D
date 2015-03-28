d29c0ad4a18a374a4713a85cb931ab99679e98bf
AlexandrePhilippi <alex.93metz@hotmail.fr>
2015-03-28 02:21:51 +0100

    probleme .gif reglé

--- Project3Donuts/src/ImageCreator/CreationFile.cpp
+++ Project3Donuts/src/ImageCreator/CreationFile.cpp
@@ -24,11 +24,14 @@ void CreationFile::pushImage(std::unique_ptr<QImage> fImage){
 
 
 void CreationFile::save(const QString& fFileName){
+
+  QString _fileName = fFileName + ".gif";
+  
   switch (mType){
   case Type::IMAGE:
     {
       if (!mImages.empty()){
-        mImages.front()->save(fFileName);
+        mImages.front()->save(_fileName);
       }
     }
     break;
@@ -37,7 +40,7 @@ void CreationFile::save(const QString& fFileName){
     {
       if (!mImages.empty()){
         GifWriter _gif;
-        _gif.GifBegin(fFileName.toStdString().c_str(), mImages.front()->width(), mImages.front()->height(), (int)mDelayBetweenFrame);
+        _gif.GifBegin(_fileName.toStdString().c_str(), mImages.front()->width(), mImages.front()->height(), (int)mDelayBetweenFrame);
 
         for (auto _im = mImages.begin(); _im != mImages.end(); ++_im){
           _gif.GifWriteFrame(*_im);
@@ -50,7 +53,7 @@ void CreationFile::save(const QString& fFileName){
 
   case Type::SEPARATED_IMAGE:
     {
-      std::string _filename = fFileName.toStdString();
+      std::string _filename = _fileName.toStdString();
 
       auto _dot = _filename.find_last_of('.');
       if (_dot != std::string::npos){
